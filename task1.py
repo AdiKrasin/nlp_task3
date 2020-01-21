4f4fca3e4d4f7fdda31441a2503362edb641236a
AdiKrasin <adikrasin@gmail.com>
2020-01-21 21:04:36 +0200

    change the SG for 1.2

--- task1.py
+++ task1.py
@@ -1,19 +1,20 @@
 import nltk
 
 sg = """
-S -> NP VP | S NP | S PREP NP
+S -> NP VP | S NP | S PREP NP | S CONJ NP
 VP -> IV | TV NP
-NP -> SNNom SN | PNNom PN | SNDet SN | PNDet PN | 'John' | 'bread' | 'Mary' | 'They' | 'her' | 'She' | 'them' | 'Everybody' | 'it' | 'butter'
+NP -> NP CONJ NP | SNNom SN | PNNom PN | SNDet SN | PNDet PN | 'John' | 'bread' | 'Mary' | 'They' | 'her' | 'She' | 'them' | 'Everybody' | 'it' | 'butter' | 'men' | 'women' | 'children' | 'men,' | 'cheese'
 SNNom -> SNDet Adj
 PNNom -> PNDet Adj
 Adj -> 'heavy'
-PREP -> 'to'
-SNDet -> 'A' | 'The' | 'a'
-PNDet -> 'many' | 'The' | 'Some'
-SN -> 'boy' | 'chair' | 'book'
+PREP -> 'to' | 'with' |'on'
+SNDet -> 'A' | 'The' | 'a' | 'the'
+PNDet -> 'many' | 'The' | 'Some' | 'the'
+SN -> 'boy' | 'chair' | 'book' | 'man' | 'telescope' | 'hill'
 PN -> 'boys'
 IV -> 'left'
-TV -> 'eats' | 'loves' | 'love' | 'gave' | 'likes' | 'moves'
+TV -> 'eats' | 'loves' | 'love' | 'gave' | 'likes' | 'moves' | 'saw' | 'knows' | 'eat'
+CONJ -> 'and'
 """
 
 g = nltk.CFG.fromstring(sg)
@@ -28,7 +29,6 @@ def parse_sentence(sent):
     trees = sr_parser.parse(tokens)
     for tree in trees:
         print(tree)
-
 
 '''
 parse_sentence("John left")
@@ -46,9 +46,21 @@ parse_sentence("A boy loves Mary")
 parse_sentence("The boy loves Mary")
 parse_sentence("Some boys love Mary")
 parse_sentence("John gave Mary a heavy book")
-'''
 parse_sentence("John gave it to Mary")
-'''
 parse_sentence("John likes butter")
 parse_sentence("John moves a chair")
+
+
+parse_sentence('John moves Mary to Mary')
+parse_sentence('John eats John')
 '''
+'''
+parse_sentence('John saw a man with a telescope')
+parse_sentence('John saw a man on the hill with a telescope')
+parse_sentence('Mary knows men and women')
+parse_sentence('Mary knows men, children and women')
+parse_sentence('John and Mary eat bread')
+parse_sentence('John and Mary eat bread with cheese')
+'''
+
+parse_sentence('John saw a man with a telescope a telescope')
