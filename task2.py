575f9d8f31d5246758ee69528931452afa69d9c9
AdiKrasin <adikrasin@gmail.com>
2020-01-22 19:27:58 +0200

    changing part 2.1 to be actual trees and not tuples - still in process

--- task2.py
+++ task2.py
@@ -1,6 +1,7 @@
 from nltk.grammar import Nonterminal
 from nltk.grammar import toy_pcfg2
 from nltk.probability import DictionaryProbDist
+from nltk.tree import Tree
 
 
 
@@ -17,6 +18,36 @@ np_probDist = DictionaryProbDist(dict)
 print(np_probDist.generate())
 '''
 
+def pcfg_generate2(grammar):
+
+    def non_terminal_into_terminal(non_terminal):
+        nt_productions = grammar.productions(Nonterminal(str(non_terminal)))
+        my_dict = dict()
+        for pr in nt_productions: my_dict[pr.rhs()] = pr.prob()
+        nt_productions_probDist = DictionaryProbDist(my_dict)
+        genereted = nt_productions_probDist.generate()
+        return genereted
+
+    # todo it's not in the same format as the tree from task1, not sure if it's ok or not - try to write it all again
+    #  same concept just with the Tree constructor
+    def nts_into_ts(genereted_nts):
+        for index in range(len(genereted_nts)):
+            old_nt = genereted_nts[index]
+            try:
+                t = non_terminal_into_terminal(genereted_nts[index])
+            except Exception as e:
+                continue
+            genereted_nts[index] = nts_into_ts(Tree(old_nt, [t]))
+        return genereted_nts
+
+    productions = grammar.productions()
+    dic = dict()
+    for pr in productions: dic[pr.rhs()] = pr.prob()
+    productions_probDist = DictionaryProbDist(dic)
+    genereted = productions_probDist.generate()
+    genereted = Tree('S', [genereted[0], genereted[1]])
+    return nts_into_ts(genereted)
+
 
 def pcfg_generate(grammar):
 
@@ -28,8 +59,6 @@ def pcfg_generate(grammar):
         genereted = nt_productions_probDist.generate()
         return genereted
 
-    # todo it does not work right recursivly when i move to the next nt i remove the old one somehow
-    # todo check if i fixed it
     def nts_into_ts(genereted_nts):
         for index in range(len(genereted_nts)):
             old_nt = genereted_nts[index]
@@ -37,7 +66,6 @@ def pcfg_generate(grammar):
                 t = non_terminal_into_terminal(genereted_nts[index])
             except Exception as e:
                 continue
-            genereted_nts = genereted_nts[:index] + ((old_nt, t),) + genereted_nts[index+1:]
             genereted_nts = genereted_nts[:index] + ((old_nt, nts_into_ts(t)),) + genereted_nts[index+1:]
         return genereted_nts
 
@@ -50,4 +78,4 @@ def pcfg_generate(grammar):
 
 
 for i in range(1000):
-    print(pcfg_generate(toy_pcfg2))
+    print(pcfg_generate2(toy_pcfg2))
