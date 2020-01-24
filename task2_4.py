0f574ee9d13853afc0b067de640d24d4a6be652c
AdiKrasin <adikrasin@gmail.com>
2020-01-24 20:56:38 +0200

    started task 2.4

--- task2_4.py
+++ task2_4.py
@@ -2,7 +2,7 @@ from nltk.corpus import LazyCorpusLoader, BracketParseCorpusReader
 from nltk import PCFG
 from nltk.grammar import ProbabilisticProduction
 from nltk import Tree, Nonterminal
-from nltk.treetransforms import chomsky_normal_form
+from nltk.probability import FreqDist
 
 
 def simplify_functional_tag(tag):
@@ -44,11 +44,7 @@ def get_productions(productions):
         else:
             probabilities[str(prod)] = 1
 
-    amount_of_interior_nodes = len([prod.lhs() for prod in productions if prod.lhs() != Nonterminal('S')])
-
     lhs_of_prods = set([prod.lhs() for prod in productions])
-
-    print('this is the amount of interior nodes: {}'.format(amount_of_interior_nodes))
 
     for lhs in lhs_of_prods:
         number_of_occurrences = 0
@@ -63,24 +59,68 @@ def get_productions(productions):
         prod = productions_to_return[index]
         productions_to_return[index] = ProbabilisticProduction(prod.lhs(), prod.rhs(),
                                                                **{'prob': probabilities[str(prod)]})
+    dist = FreqDist(probabilities)
+    dist.plot(len(probabilities))
 
     return productions_to_return
 
 
-def pcfg_learn(treebank, n):
+def pcfg_learn1(treebank, n):
     productions = list()
     for i in range(n):
         for tree in treebank.parsed_sents()[:i+1]:
             prod_gen = tree_to_productions(tree)
             tree_to_append = next(prod_gen)
             while tree_to_append:
-                productions.append(tree_to_append)
+                if tree_to_append.lhs() == Nonterminal('NP'):
+                    productions.append(tree_to_append)
                 try:
                     tree_to_append = next(prod_gen)
                 except Exception as e:
                     tree_to_append = False
     productions = get_productions(productions)
-    return PCFG(Nonterminal('S'), productions)
+    return PCFG(Nonterminal('NP'), productions)
 
 
-print(pcfg_learn(treebank, 200))
+def pcfg_learn2(treebank, n):
+    productions = list()
+    for i in range(n):
+        for tree in treebank.parsed_sents()[:i+1]:
+            prod_gen = tree_to_productions(tree)
+            tree_to_append = next(prod_gen)
+            father_title = Nonterminal('BOT')
+            # todo i am taking the father title in the wrong way because i am not getting vp as father of np -
+            #  looked on the method and understood why very easily - need to maybe modify the method. i can yield the
+            #  parent's label as well and then little modifications will be required
+            while tree_to_append:
+                if tree_to_append.lhs() == Nonterminal('NP') and father_title == Nonterminal('S'):
+                    productions.append(tree_to_append)
+                try:
+                    father_title = tree_to_append.lhs()
+                    tree_to_append = next(prod_gen)
+                except Exception as e:
+                    tree_to_append = False
+    productions = get_productions(productions)
+    return PCFG(Nonterminal('NP'), productions)
+
+
+def pcfg_learn3(treebank, n):
+    productions = list()
+    for i in range(n):
+        for tree in treebank.parsed_sents()[:i+1]:
+            prod_gen = tree_to_productions(tree)
+            tree_to_append = next(prod_gen)
+            father_title = Nonterminal('BOT')
+            while tree_to_append:
+                if tree_to_append.lhs() == Nonterminal('NP') and father_title == Nonterminal('VP'):
+                    productions.append(tree_to_append)
+                try:
+                    father_title = tree_to_append.lhs()
+                    tree_to_append = next(prod_gen)
+                except Exception as e:
+                    tree_to_append = False
+    productions = get_productions(productions)
+    return PCFG(Nonterminal('NP'), productions)
+
+
+print(pcfg_learn3(treebank, 200))
