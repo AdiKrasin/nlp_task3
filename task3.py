ef361206b0c9742715170d0fe5c11ad9c3c90e07
AdiKrasin <adikrasin@gmail.com>
2020-01-26 19:38:48 +0200

    finished task 3.1 (with 400 instead of 3200 trees as training trees)

---
+++ task3.py
@@ -0,0 +1,89 @@
+from nltk.parse.viterbi import ViterbiParser
+from nltk.corpus import LazyCorpusLoader, BracketParseCorpusReader
+from nltk import PCFG
+from nltk.grammar import ProbabilisticProduction
+from nltk import Tree, Nonterminal
+from nltk.treetransforms import chomsky_normal_form
+
+
+def simplify_functional_tag(tag):
+    if '-' in tag:
+        tag = tag.split('-')[0]
+    return tag
+
+
+def get_tag(tree):
+    if isinstance(tree, Tree):
+        return Nonterminal(simplify_functional_tag(tree.label()))
+    else:
+        return tree
+
+
+def tree_to_production(tree):
+    return ProbabilisticProduction(get_tag(tree), [get_tag(child) for child in tree], **{'prob': 0})
+
+
+def tree_to_productions(tree):
+    yield tree_to_production(tree)
+    for child in tree:
+        if isinstance(child, Tree):
+            for prod in tree_to_productions(child):
+                yield prod
+
+
+treebank = LazyCorpusLoader('treebank/combined', BracketParseCorpusReader, r'wsj_.*\.mrg')
+
+
+def get_productions(productions):
+
+    probabilities = dict()
+    productions_to_return = list(set(productions))
+
+    for prod in productions:
+        if str(prod) in probabilities:
+            probabilities[str(prod)] += 1
+        else:
+            probabilities[str(prod)] = 1
+
+    lhs_of_prods = set([prod.lhs() for prod in productions])
+
+    for lhs in lhs_of_prods:
+        number_of_occurrences = 0
+        for prob in probabilities:
+            if prob.startswith(str(lhs) + " "):
+                number_of_occurrences += probabilities[prob]
+        for prob in probabilities:
+            if prob.startswith(str(lhs) + " "):
+                probabilities[prob] = probabilities[prob] / number_of_occurrences
+
+    for index in range(len(productions_to_return)):
+        prod = productions_to_return[index]
+        productions_to_return[index] = ProbabilisticProduction(prod.lhs(), prod.rhs(),
+                                                               **{'prob': probabilities[str(prod)]})
+
+    return productions_to_return
+
+
+def pcfg_learn(treebank, n):
+    productions = list()
+    for i in range(n):
+        for tree in treebank.parsed_sents()[:i+1]:
+            chomsky_normal_form(tree, factor='right', horzMarkov=1, vertMarkov=1, childChar='|', parentChar='^')
+            prod_gen = tree_to_productions(tree)
+            tree_to_append = next(prod_gen)
+            while tree_to_append:
+                productions.append(tree_to_append)
+                try:
+                    tree_to_append = next(prod_gen)
+                except Exception as e:
+                    tree_to_append = False
+    productions = get_productions(productions)
+    return PCFG(Nonterminal('S'), productions)
+
+
+pcfg_training = pcfg_learn(treebank, 400)
+parser = ViterbiParser(pcfg_training)
+
+# this sentence is from the testing trees
+print(parser.parse_all('Pierre Vinken , 61 years old , will join the board as a nonexecutive director Nov. 29'
+                       ' .'.split()))
