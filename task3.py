192131d9058c083b2aa2fad1cea98bfc0f921698
AdiKrasin <adikrasin@gmail.com>
2020-01-30 11:19:10 +0200

    might have finished 3.2

--- task3.py
+++ task3.py
@@ -4,6 +4,7 @@ from nltk import PCFG
 from nltk.grammar import ProbabilisticProduction
 from nltk import Tree, Nonterminal
 from nltk.treetransforms import chomsky_normal_form
+import numpy as np
 
 
 def simplify_functional_tag(tag):
@@ -81,32 +82,28 @@ def pcfg_learn(treebank, n):
     return PCFG(Nonterminal('S'), productions)
 
 
-#pcfg_training = pcfg_learn(treebank, 400)
-#parser = ViterbiParser(pcfg_training)
+pcfg_training = pcfg_learn(treebank, 400)
+parser = ViterbiParser(pcfg_training)
+
+# todo delete this print:
+print('finished1')
 
 
-def find_first_index(parse_tree):
-    if isinstance(parse_tree[0], Tree):
-        return find_first_index(parse_tree[0])
-    return parse_tree[0]
-
-
-def find_last_index(parse_tree):
-    if isinstance(parse_tree[-1], Tree):
-        return find_last_index(parse_tree[-1])
-    return parse_tree[-1][0]
-
-
-def get_list_of_labelled_constituents(parse_tree):
-    lst_to_return = list()
-    label = parse_tree.label()
-    first_index = find_first_index(parse_tree)
-    last_index = find_last_index(parse_tree)
-    lst_to_return.append((label, first_index, last_index))
-    for child in parse_tree:
-        if isinstance(child, Tree):
-            lst_to_return.append(get_list_of_labelled_constituents(child))
-    return lst_to_return
+def get_list_of_labelled_constituents(parse_tree, lst=None, first_index=None, last_index=None):
+    if lst is None:
+        return get_list_of_labelled_constituents(parse_tree, list(), 0, len(parse_tree.leaves()) - 1)
+    if not len(lst):
+        lst = [(parse_tree.label(), first_index, last_index)]
+    if len(list(parse_tree.subtrees())) == 1:
+        return [(parse_tree.label(), first_index, last_index)]
+    else:
+        for child in parse_tree:
+            labelled_constituents = (child.label(), first_index, first_index + len(child.leaves()) - 1)
+            get_list_of_labelled_constituents(child, lst, first_index, first_index + len(child.leaves()) - 1)
+            first_index += len(child.leaves())
+            last_index += len(child.leaves())
+            lst.append(labelled_constituents)
+        return lst
 
 
 def get_test_dataset(treebank, n):
@@ -115,6 +112,8 @@ def get_test_dataset(treebank, n):
         for tree in treebank.parsed_sents()[:i+1]:
             chomsky_normal_form(tree, factor='right', horzMarkov=1, vertMarkov=1, childChar='|', parentChar='^')
             test_trees.append(tree)
+        if len(test_trees) == 50:
+            return test_trees
     return test_trees
 
 
@@ -128,11 +127,80 @@ def turn_tree_into_sentence(tree):
     return sentence
 
 
-# todo change to 200 instead of 1 when finish developing
-tree = get_test_dataset(treebank, 1)
-print(tree[0])
-print('\n')
-print(turn_tree_into_sentence(tree[0]))
+trees = get_test_dataset(treebank, 50)
+# todo delete this print
+print('finished2')
+sentences = list()
+for tree in trees:
+    sentences.append(turn_tree_into_sentence(tree))
+    if len(sentences) == 50:
+        break
+# todo delete this print and count
+print('finished3')
+count = 0
+gen_trees = list()
+for sentence in sentences:
+    gen_tree = parser.parse_all(sentence.split())
+    # todo delete this print and count
+    count += 1
+    print('finished {} out of {}'.format(count, len(sentences)))
+    if len(gen_tree):
+        gen_trees.append(gen_tree[0])
+# todo delete this print
+print('finished4')
+list_of_labelled_constituents_for_trees = list()
+for tree in trees:
+    list_of_labelled_constituents_for_trees.append(get_list_of_labelled_constituents(tree))
+    if len(list_of_labelled_constituents_for_trees) == 50:
+        break
+# todo delete this print
+print('finished5')
+list_of_labelled_constituents_for_gen_trees = list()
+for tree in gen_trees:
+    list_of_labelled_constituents_for_gen_trees.append(get_list_of_labelled_constituents(tree))
+    if len(list_of_labelled_constituents_for_gen_trees) == 50:
+        break
 
-# todo need to get_list_of_labelled_constituents for the testing trees, then turn into sentences and parse and then
-#  get_list_of_labelled_constituents for the parsed trees and compare
+
+def get_unlabelled_precision_and_recall(labelled_constituents1, labelled_constituents2):
+    precisions = list()
+    recalls = list()
+    for index in range(len(labelled_constituents1)):
+        labelled_constituent1 = labelled_constituents1[index]
+        labelled_constituent2 = labelled_constituents2[index]
+        number_of_correct_constituents = 0
+        number_of_total_constituents = len(labelled_constituent1)
+        number_of_total_constituents2 = len(labelled_constituent2)
+        for index2 in range(len(labelled_constituent1)):
+            if labelled_constituent1[index2][1] == labelled_constituent2[index2][1]:
+                if labelled_constituent1[index2][2] == labelled_constituent2[index2][1]:
+                    number_of_correct_constituents += 1
+        precisions.append(number_of_correct_constituents / number_of_total_constituents)
+        recalls.append(number_of_correct_constituents / number_of_total_constituents2)
+    return [np.mean(precisions), np.mean(recalls),
+            (2*np.mean(precisions)*np.mean(recalls)) / (np.mean(precisions) + np.mean(recalls))]
+
+
+def get_labelled_precision_and_recall(labelled_constituents1, labelled_constituents2):
+    precisions = list()
+    recalls = list()
+    for index in range(len(labelled_constituents1)):
+        labelled_constituent1 = labelled_constituents1[index]
+        labelled_constituent2 = labelled_constituents2[index]
+        number_of_correct_constituents = 0
+        number_of_total_constituents = len(labelled_constituent1)
+        number_of_total_constituents2 = len(labelled_constituent2)
+        for index2 in range(len(labelled_constituent1)):
+            if labelled_constituent1[index2][0] == labelled_constituent2[index2][0]:
+                number_of_correct_constituents += 1
+        precisions.append(number_of_correct_constituents / number_of_total_constituents)
+        recalls.append(number_of_correct_constituents / number_of_total_constituents2)
+    return [np.mean(precisions), np.mean(recalls),
+            (2*np.mean(precisions)*np.mean(recalls)) / (np.mean(precisions) + np.mean(recalls))]
+
+
+print(get_labelled_precision_and_recall(list_of_labelled_constituents_for_gen_trees,
+                                        list_of_labelled_constituents_for_trees))
+
+print(get_unlabelled_precision_and_recall(list_of_labelled_constituents_for_gen_trees,
+                                          list_of_labelled_constituents_for_trees))
